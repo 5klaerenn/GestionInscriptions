e6df42f0d0b12f01a81124dbf85d34af2fdcb5ac
5klaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-07-17 11:03:36 -0400

    maj menus main

--- GestionNotes/src/main/java/org/sklaerenn/demo/DaoDemo.java
+++ GestionNotes/src/main/java/org/sklaerenn/demo/DaoDemo.java
@@ -7,6 +7,7 @@ import org.sklaerenn.model.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class DaoDemo {
     public static void main(String[] args) throws SQLException {
@@ -15,61 +16,89 @@ public class DaoDemo {
         EtudiantDao etuDao = new EtudiantDaoImpl();
         NotesDao notesDao = new NotesDaoImpl();
 
-//        Etudiant e1 = new Etudiant("Kelso", "Bob");
-//        Etudiant e2 = new Etudiant("Cox", "Perry");
-//        Etudiant e3 = new Etudiant("Dorian", "John");
-//        Etudiant e4 = new Etudiant("Turkelton", "Turk");
-//        Etudiant e5 = new Etudiant("Reid", "Eliott");
-//        etuDao.create(e1);
-//        etuDao.create(e2);
-//        etuDao.create(e3);
-//        etuDao.create(e4);
-//        etuDao.create(e5);
-//
-//        Cours c1 = new Cours("420-A34-RO", "Approf. bases de données");
-//        Cours c2 = new Cours("420-977-RO", "Structure de données");
-//        Cours c3 = new Cours("420-994-RO", "UI/UX");
-//        coursDao.create(c1);
-//        coursDao.create(c2);
-//        coursDao.create(c3);
 
-//        Note n1 = new Note("DORJ", "420-A34-RO", 67, 83);
-//        Note n2 = new Note("DORJ", "420-977-RO", 88, 92);
-//        Note n3 = new Note("DORJ", "420-994-RO", 72, 95);
-//        notesDao.create(n1);
-//        notesDao.create(n2);
-//        notesDao.create(n3);
-
-        List<Note> noteList = notesDao.findAll();
-        for(Note n : noteList){
-            System.out.println(n);
-        }
-
-        System.out.println("******TEST CONSTRUCTEUR 2 PARAMS******");
-        Note n4 =  new Note("COXP", "420-A34-RO");
-//        notesDao.create(n4);
-
-        noteList = notesDao.findAll();
-        for(Note n : noteList){
-            System.out.println(n);
-        }
-
-        n4.setNote1(44);
-        notesDao.update(n4);
-        System.out.println(notesDao.findById("COXP", "420-A34-RO"));
-
-        System.out.println();
-        n4.setNote2(78);
-        notesDao.update(n4);
-        System.out.println(notesDao.findById("COXP", "420-A34-RO"));
-
-        notesDao.delete("COXP", "420-A34-RO");
-        System.out.println();
-        noteList = notesDao.findAll();
-        for(Note n : noteList){
-            System.out.println(n);
-        }
 
 
     }
+
+    public void menuPrincipal(){
+
+        boolean exit = false;
+        int choix;
+
+        while(!exit) {
+            System.out.println("************MENU PRINCIPAL************");
+            System.out.println("1. Étudiant");
+            System.out.println("2. Cours");
+            System.out.println("3. Notes");
+            System.out.println("4. Sortir");
+
+            choix = estEntierPositif();
+
+            switch (choix) {
+                case 1:
+                    subEtudiant();
+                    break;
+                case 2:
+                    subCours();
+                    break;
+                case 3:
+                    subNotes();
+                    break;
+                case 4:
+                    exit = true;
+                    System.out.println("Merci !");
+                    break;
+                default:
+                    System.out.println("Option invalide. Réessayez");
+                    break;
+            }
+    }
+
+    }
+
+    public void subEtudiant(){
+
+        boolean exit = false;
+        int choix;
+
+        while(!exit){
+            System.out.println("************MENU ÉTUDIANT************");
+            System.out.println("1. Ajouter un étudiant");
+            System.out.println("2. Afficher tous les étudiants");
+            System.out.println("3. Rechercher un étudiant");
+            System.out.println("4. Supprimer un étudiant");
+            System.out.println("5. Mettre à jour un étudiant");
+            System.out.println("6. Retour au menu principal");
+
+            choix = estEntierPositif();
+
+        //switch (choix)
+
+        }
+
+    }
+    public void subCours(){
+
+    }
+    public void subNotes(){
+
+    }
+
+    static int estEntierPositif() {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            while(sc.hasNextInt()) {
+                int test = sc.nextInt();
+                if (test >= 0) {
+                    return test;
+                }
+            }
+
+            System.out.println("Erreur, entrez un entier positif");
+            sc.next();
+        }
+    }
+
 }

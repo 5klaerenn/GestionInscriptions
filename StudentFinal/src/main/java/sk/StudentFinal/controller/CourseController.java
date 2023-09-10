047467d37ee0b5a173ba8a6c76eee08a5c666699
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-09-10 13:51:23 -0400

    implementation de add, delete et update pour les courses.

--- StudentFinal/src/main/java/sk/StudentFinal/controller/CourseController.java
+++ StudentFinal/src/main/java/sk/StudentFinal/controller/CourseController.java
@@ -3,14 +3,13 @@ package sk.StudentFinal.controller;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sk.StudentFinal.Service.CourseServiceImpl;
-import sk.StudentFinal.dao.CourseRepository;
 import sk.StudentFinal.model.Course;
+import sk.StudentFinal.model.Student;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/app")
@@ -19,17 +18,42 @@ public class CourseController {
     private CourseServiceImpl courseService;
     public CourseController(CourseServiceImpl courseService){this.courseService = courseService;}
 
-    private List<Course> courseList;
-
-    @PostConstruct
-    public void loadData(){
-        courseList = courseService.findAll();
-    }
-
     @GetMapping("/courses")
     public String afficherTout(Model model) {
+        List<Course> courseList = courseService.findAll();
         model.addAttribute("courses", courseList);
-        return "list-courses";
+        return "courses/list-courses";
+    }
+
+    @GetMapping("/newcourse")
+    public String courseForm(Model model) {
+        model.addAttribute("course", new Course());
+        return "courses/form-courses";
+    }
+
+    @GetMapping("/updatecourse")
+    public String courseUpdateForm(@RequestParam("course_id") String id, Model model) {
+        Optional<Course> course = courseService.findById(id);
+        model.addAttribute("course", course);
+        return "courses/form-updatecourses";
+    }
+
+    @GetMapping("/deletecourse")
+    public String deleteStudent(@RequestParam("course_id") String id) {
+        courseService.deleteById(id);
+        return "redirect:/app/courses";
+    }
+
+    @PostMapping("/courses")
+    public String saveStudent(@ModelAttribute("course") Course newCourse){
+        courseService.save(newCourse);
+        return "redirect:/app/courses";
+    }
+
+    @PostMapping("/update_course")
+    public String saveUpdatedStudent(@ModelAttribute("course") Course updcourse){
+        courseService.save(updcourse);
+        return "redirect:/app/courses";
     }
 
 
